e136d790a49802b10743f815d137929d25d51f74
lizhaowei <444856601@qq.com>
2022-06-28 20:59:58 +0800

    easy:11

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,11 @@
-type TupleToObject<T extends readonly any[]> = any
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Liii_zw
+ * @Date: 2022-06-20 20:34:32
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-06-28 20:59:45
+ */
+type TupleToObject<T extends readonly any[]> = {
+ [K in  T[number]]:K
+}
