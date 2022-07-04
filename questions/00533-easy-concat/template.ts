5fb600e1a951530cf32b84370e3c04c4e54fa81b
lizhaowei <444856601@qq.com>
2022-07-04 15:27:26 +0800

    easy:533,898

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,2 @@
-type Concat<T, U> = any
+type Concat<T extends any[], U extends any[]> = [...T,...U]
+type aaa = Concat<[1,2,3],[2,3,4]>
--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,13 @@
-type Includes<T extends readonly any[], U> = any
+type IsEqual<T, U> = (<G>() => G extends T ? 1 : 2) extends <G>() => G extends U
+  ? 1
+  : 2
+  ? true
+  : false
+
+type Includes<T extends readonly any[], U> = T extends [infer F, ...infer Rest]
+? IsEqual<F, U> extends true
+  ? true
+  : Includes<Rest, U>
+: false
+
+type a1 = Includes<[1], 1 | 2 | 3>
